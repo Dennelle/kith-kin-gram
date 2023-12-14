c08cc8c20381f36c65a0d4c71d5d3e9d3a12f9f7
Dennelle <dennelle.walton@gmail.com>
2023-12-13 21:58:51 -0500

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,65 @@
-import { Route, Routes } from "react-router-dom";
-// import "./App.css";
+import { useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
 
-// import userService from "./utils/userService";
+import userService from "./utils/userService";
 
 // // ===== pages ======
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignupPage/SignupPage";
+import FeedPage from "./pages/FeedPage/FeedPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 // ===== components ======
+function App() {
+  const [user, setUser] = useState(userService.getUser());
 
-export default function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
+  function handleSignUpOrLogin() {
+    setUser(userService.getUser());
+  }
+
+  function logout() {
+    console.log("happening");
+    userService.logout();
+    setUser(null);
+  }
+
+  if (!user) {
+    return (
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/login"
+          element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUpPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
-    </>
+    );
+  }
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<FeedPage loggedUser={user} handleLogout={logout} />}
+      />
+      <Route
+        path="/login"
+        element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+      />
+      <Route
+        path="/signup"
+        element={<SignUpPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+      />
+      <Route
+        path="/:username"
+        element={<ProfilePage loggedUser={user} handleLogout={logout} />}
+      />
+    </Routes>
   );
 }
+
+export default App;
